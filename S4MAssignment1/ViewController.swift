30313d3c40aae385c2f02e8820cb78cbef5449ef
mohammed shafiullah <mohammedshafiullah@mohammeds-macbook-pro.local>
2017-10-25 02:31:42 +0530

    Initial Commit

---
+++ S4MAssignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S4MAssignment1
+//
+//  Created by mohammed shafiullah on 25/10/17.
+//  Copyright © 2017 mohammed shafiullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
